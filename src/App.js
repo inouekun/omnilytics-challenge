38fca8c8e759fa6897fad3450b8d870403803a38
Ku Mohd Syafiq <kumsyafiq@gmail.com>
2021-05-17 10:51:59 +0800

    complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import './App.css';
+import { genAlphas, genNumbers } from './utility';
 
 let downloadFlag = false;
 
@@ -13,21 +14,36 @@ function App() {
   };
 
   const [link, setLink] = useState(null);
-  const [report, setReport] = useState(0);
+  const [report, setReport] = useState(false);
   const [result, setResult] = useState(null);
   const [reportDetails, setReportDetails] = useState(initialState);
 
-  const resetReportDetails = async () => {
-    setReportDetails(initialState);
+  useEffect(() => {
+    if (result && !downloadFlag) {
+      downloadTxtFile();
+      downloadFlag = true; // Set flag so wont download multiple times
+    }
+  }, [result, downloadFlag]);
+
+  const downloadTxtFile = () => {
+    const element = document.createElement('a');
+    const file = new Blob([result], { type: 'text/plain' });
+    element.href = URL.createObjectURL(file);
+
+    element.download = 'result.txt';
+    document.body.appendChild(element); // Required for this to work in FireFox
+    setLink(element.href); // Display link
+    return element.click();
   };
 
-  const genData = async () => {
+  const genData = () => {
     let result = [];
-    resetReportDetails();
+    let stringResult = '';
 
     // 100 - 8KB
     // 26000 - 2MB
-    for (let i = 0; i < 26000; i++) {
+    // 103996 length of Array - 2MB
+    for (let i = 1; i < 26000; i++) {
       result.push(genAlphas(true)); // Alphabetical strings
       result.push(genNumbers(false)); // Real numbers
       result.push(genAlphas(false)); // Alphanumerics
@@ -38,57 +54,19 @@ function App() {
         integers: reportDetails.integers + i,
         alphanumerics: reportDetails.alphanumerics + i,
       });
+
+      /*
+          Cant do checking if exceed 2MB, will hang
+          If below works, will randomize output quantity from genAlphas and genNumbers
+      */
+      // if (result.toString().length >= 2000000) {
+      //   break;
+      // }
     }
 
-    setResult(result);
+    const newResult = result.toString(); // String length 1998775 ~ 2MB
+    setResult(newResult);
   };
-
-  // 26 bytes - Letters only
-  // 36 bytes - Alphanumerics
-  const genAlphas = (lettersOnly) => {
-    const randomLetters = 'abcdefghijklmnopqrstuvwxyz';
-    const randomAlphanumerics = 'abcdefghijklmnopqrstuvwxyz1234567890';
-    let result = '';
-
-    if (lettersOnly) {
-      for (var i = 0; i < randomLetters.length; i++) {
-        result += randomLetters.charAt(Math.floor(Math.random() * randomLetters.length));
-      }
-    } else {
-      for (var j = 0; j < randomAlphanumerics.length; j++) {
-        result += randomAlphanumerics.charAt(Math.floor(Math.random() * randomAlphanumerics.length));
-      }
-    }
-    return result;
-  };
-
-  // 5 bytes - whole numbers
-  // 7 bytes - integers
-  const genNumbers = (decimal) => {
-    const result = Math.floor(Math.random() * 5000) + 1000; // Math.floor(Math.random() * (max - min + 1)) + min;
-    const precision = 100; // 2 decimals
-    const resultWithDecimal = Math.floor(Math.random() * (5000 * precision - 1 * precision) + 1000 * precision) / (1 * precision);
-
-    return decimal ? resultWithDecimal : result;
-  };
-
-  const downloadTxtFile = async () => {
-    const element = document.createElement('a');
-    const file = new Blob([result], { type: 'text/plain' });
-    element.href = URL.createObjectURL(file);
-
-    element.download = 'result.txt';
-    document.body.appendChild(element); // Required for this to work in FireFox
-    setLink(element.href); // display link
-    element.click();
-  };
-
-  useEffect(() => {
-    if (result && !downloadFlag) {
-      downloadTxtFile();
-      downloadFlag = true;
-    }
-  }, [result, downloadFlag]);
 
   return (
     <div className='container'>
@@ -97,9 +75,10 @@ function App() {
           <Button
             variant='primary'
             onClick={() => {
-              downloadFlag = false; // reset flag
+              downloadFlag = false; // Reset flag
               genData();
             }}
+            disabled={result}
           >
             Generate
           </Button>
@@ -113,10 +92,10 @@ function App() {
               </a>
             )}
           </p>
-          <Button variant='primary' onClick={() => setReport(1)}>
+          <Button variant='primary' disabled={report} onClick={() => setReport(true)}>
             Report
           </Button>
-          {report === 1 ? (
+          {report ? (
             <p>
               Alphabetical string: {reportDetails.alphabetical}
               <br />
--- src/utility.js
+++ src/utility.js
@@ -1,9 +1,28 @@
-const downloadTxtFile = () => {
-  console.log('test');
-  const element = document.createElement('a');
-  const file = new Blob([document.getElementById('myInput').value], { type: 'text/plain' });
-  element.href = URL.createObjectURL(file);
-  element.download = 'result.txt';
-  document.body.appendChild(element); // Required for this to work in FireFox
-  element.click();
+// 26 bytes - Letters only
+// 36 bytes - Alphanumerics
+export const genAlphas = (lettersOnly) => {
+  const randomLetters = 'abcdefghijklmnopqrstuvwxyz';
+  const randomAlphanumerics = 'abcdefghijklmnopqrstuvwxyz1234567890';
+  let result = '';
+
+  if (lettersOnly) {
+    for (var i = 0; i < randomLetters.length; i++) {
+      result += randomLetters.charAt(Math.floor(Math.random() * randomLetters.length));
+    }
+  } else {
+    for (var j = 0; j < randomAlphanumerics.length; j++) {
+      result += randomAlphanumerics.charAt(Math.floor(Math.random() * randomAlphanumerics.length));
+    }
+  }
+  return result;
+};
+
+// 5 bytes - Whole numbers
+// 7 bytes - Integers
+export const genNumbers = (decimal) => {
+  const result = Math.floor(Math.random() * 5000) + 1000; // Math.floor(Math.random() * (max - min + 1)) + min;
+  const precision = 100; // 2 decimals
+  const resultWithDecimal = Math.floor(Math.random() * (5000 * precision - 1 * precision) + 1000 * precision) / (1 * precision);
+
+  return decimal ? resultWithDecimal : result;
 };
